978e4914dfb525ce77d3301d504c3bf9d41644eb
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2021-09-21 00:49:18 +0530

    setting up training scripts

--- backend/services/text_similarity/application/ai/training/src/train.py
+++ backend/services/text_similarity/application/ai/training/src/train.py
@@ -1,8 +1,12 @@
+import pandas as pd
+from sklearn import model_selection
+
 from services.text_similarity.application.ai.model import BERTClassifier
+from services.text_similarity.application.ai.training.src import utils
+from services.text_similarity.application.ai.training.src.dataset import BERTDataset
 from services.text_similarity.application.ai.training.src.engine import Engine
 from services.text_similarity.application.ai.training.src.preprocess import Preprocess
 from services.text_similarity.settings import Settings
-
 
 from transformers import AdamW, get_linear_schedule_with_warmup
 from torch.utils.data import DataLoader
@@ -14,6 +18,8 @@ class Train:
         self.settings = Settings
         self.engine = Engine()
         self.preprocess = Preprocess()
+        self.early_stopping = utils.EarlyStopping(patience=self.settings.patience,
+                                                  mode=self.settings.mode)
 
         # initialize required variables
         self.bert_text_model = None
@@ -61,4 +67,42 @@ class Train:
             },
         ]
 
+    def __create_data_loaders(self, sentence1, sentence2, targets, batch_size, num_workers):
+        dataset = BERTDataset(sentence_1=sentence1,
+                              sentence_2=sentence2,
+                              targets=targets)
+        data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+
+        return data_loader
+
+    def __load_data(self, csv_data_path):
+        df = pd.read_csv(csv_data_path).dropna().reset_index(drop=True)
+        df_train, df_valid = model_selection.train_test_split(
+            df,
+            random_state=self.settings.seed_value,
+            test_size=self.settings.test_size,
+            stratify=df.is_duplicate.values
+
+        )
+
+        df_train = df_train.reset_index(drop=True)
+        df_valid = df_valid.reset_index(drop=True)
+
+        # creating Data Loaders
+        # train data loader
+        self.train_data_loader = self.__create_data_loaders(sentence1=df_train.question1.values,
+                                                            sentence2=df_train.question2.values,
+                                                            targets=df_train.is_duplicate.values,
+                                                            num_workers=self.settings.TRAIN_NUM_WORKERS)
+
+        # validation data loader
+        self.val_data_loader = self.__create_data_loaders(sentence1=df_valid.question1.values,
+                                                          sentence2=df_valid.question2.values,
+                                                          targets=df_valid.is_duplicate.values,
+                                                          num_workers=self.settings.VAL_NUM_WORKERS)
+
+        self.total_steps = int(len(df_train) / self.settings.TRAIN_BATCH_SIZE * self.settings.EPOCHS)
+
+    def train(self):
+        pass
 
--- backend/services/text_similarity/settings.py
+++ backend/services/text_similarity/settings.py
@@ -43,3 +43,8 @@ class Settings:
     VALID_BATCH_SIZE = 16
     EPOCHS = 10
     RANDOM_STATE = 42
+    TRAIN_NUM_WORKERS = 4
+    VAL_NUM_WORKERS = 2
+    patience = 4
+    mode = "max"
+
