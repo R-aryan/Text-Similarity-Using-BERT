7045970ca52523544c3f2c80e373625f001c541a
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2021-09-22 18:29:53 +0530

    testing training scripts

--- backend/services/text_similarity/application/ai/model.py
+++ backend/services/text_similarity/application/ai/model.py
@@ -36,3 +36,24 @@ class BERTClassifier(nn.Module):
         output = self.out(bo)
 
         return output
+
+    def print_model_details(self):
+        # Get all of the model's parameters as a list of tuples.
+        params = list(self.bert.named_parameters())
+
+        print('The BERT Base Uncased Model Has {:} different named parameters.\n'.format(len(params)))
+
+        print('==== Embedding Layer ====\n')
+
+        for p in params[0:5]:
+            print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+
+        print('\n==== First Transformer ====\n')
+
+        for p in params[5:21]:
+            print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+
+        print('\n==== Output Layer ====\n')
+
+        for p in params[-4:]:
+            print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
